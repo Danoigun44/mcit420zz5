afb7e20685530ceca2d2bf4084c8b9509511ccfb
Sami Islam <islam.samipkr@gmail.com>
2024-05-29 16:53:35 -0400

    Update threetiermodule.tf

--- threetiermodule.tf
+++ threetiermodule.tf
@@ -14,13 +14,10 @@ output "module_web_vm_public_ip" {
 
 module "three_tier_app_count" {
   source              = "./modulethreetierapp_count/"
-  resource_group_name = "mcitthreetiermodule"
+  resource_group_name = "mcitthreetiermodulecount"
   location            = "West Europe"
   admin_username      = var.administrator_login
   admin_password      = var.administrator_login_password
-  db_admin_username   = var.db_admin_username
-  db_admin_password   = var.db_admin_password
-
   count               = var.instance_count
 }
 
